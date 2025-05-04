62c65ea4c0fa9dbc4e7180ebfd10939d029f2efa
sashavershkova <a.m.vershkova@gmail.com>
2025-05-03 23:35:59 -0700

    add set1 and set2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,35 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if parens == "()" or parens == "":
+        return True
+    if parens == "((" or parens == "))" or len(parens) == 1:
+        return False
 
+    return is_nested_parens(parens[1:-1])
+    
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,42 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if len(array) == 0:
+        return False
+    
+    if array[0] == query:
+        return True
+    
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) == 1 or len(text) == 0:
+        return True
+    
+    if text[0] != text[-1]:
+        return False
+        
+    return is_palindrome(text[1:-1])
+        
 
 
 # digit_match
-
+def digit_match(int1, int2):  
+    count = 0
+    # base case when we compare just one digit with last digit of a longer number
+    # this is where our recursion will stop
+    if int1 // 10 == 0 or int2 // 10 == 0:
+        if int1%10 == int2%10:
+            return 1
+        else:
+            return 0
+        
+    # recursive case    
+    if int1%10 == int2%10:
+        count = 1
+ 
+    return (count + digit_match(int1//10, int2//10))
 
